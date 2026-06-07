8fb31c0d9861c7d8df9fa35565170f0c4001cd95
Sri Naga Prahalad Pillutla <srinp131@gmail.com>
2026-06-07 00:05:31 -0700

    w

--- realtime_signals.py
+++ realtime_signals.py
@@ -762,4 +762,6 @@ if __name__ == "__main__":
     t = threading.Thread(target=background_refresh, daemon=True)
     t.start()
 
-    app.run(host="0.0.0.0", port=5000, debug=False)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=False)
